57a413920902fd588e68269508d72c2669efdc1e
ElMehdiEchchad <elmehdiechchad@gmail.com>
2021-01-17 15:41:45 +0100

    Second commit. (CALL_PHONE action added instead of dial)

--- app/src/main/java/com/tp/notificationownessay/MainActivity.java
+++ app/src/main/java/com/tp/notificationownessay/MainActivity.java
@@ -1,21 +1,28 @@
 package com.tp.notificationownessay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String CHANNEL_ID = "channel1";
+    private static final int reqCode=1;
 
 
     @Override
@@ -29,7 +36,15 @@ public class MainActivity extends AppCompatActivity {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             final int importance = NotificationManager.IMPORTANCE_DEFAULT;
             //We create an intent to dial a number
-            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0606060606"));
+            if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    /*
+                    Appel à la méthode AskForPermission() pour que l'utilisateur donne la
+                    permission manuellement
+                    */
+            askPermission();}
+            else{
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:0606060606"));
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, nameC, importance);
             channel.setDescription("a test channel");
             NotificationCompat.Builder notifBuilder;
@@ -42,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager mNotificationManager = (NotificationManager)
                     getSystemService(Context.NOTIFICATION_SERVICE);
             mNotificationManager.createNotificationChannel(channel);
-            mNotificationManager.notify(01, notifBuilder.build());
-        } else {
+            mNotificationManager.notify(01, notifBuilder.build());}
+        } /*else {
             final int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationCompat.Builder notifBuilder;
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:0606060606"));
@@ -54,6 +69,30 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager mNotificationManager = (NotificationManager)
                     getSystemService(Context.NOTIFICATION_SERVICE);
             mNotificationManager.notify(02, notifBuilder.build());
+
+            //this bloc serves for the API level below 26 so it's not necessary to call it here for NEXUS API=30
+        }*/
+    }
+    public void askPermission(){
+        //This bloc of condition is excecuted in case of denial of permission by the user for the first time
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CALL_PHONE)) {
+            new AlertDialog.Builder(MainActivity.this).setTitle("test").setMessage("permission pour pouvoir passer l'appel que" +
+                    "vous voulez" +
+                    "").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, reqCode);
+                }
+
+            }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_LONG).show();
+                }
+            }).create().show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, reqCode);
         }
     }
 
